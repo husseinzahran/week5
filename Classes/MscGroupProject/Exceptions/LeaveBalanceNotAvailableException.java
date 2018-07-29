e1c726401a0cd1718d3135c11e767f7e9b56da4f
husseinzahran <hussein.zahran@crossover.com>
2018-07-29 23:19:11 +0400

    add exceptions

---
+++ Classes/MscGroupProject/Exceptions/LeaveBalanceNotAvailableException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MscGroupProject.Exceptions;
+
+/**
+ *
+ * @author husseinzahran
+ */
+public class LeaveBalanceNotAvailableException extends Exception {
+    public LeaveBalanceNotAvailableException(String message) {
+        super(message);
+    }
+}
--- Classes/MscGroupProject/Manager.java
+++ Classes/MscGroupProject/Manager.java
@@ -4,10 +4,43 @@
  * and open the template in the editor.
  */
 package MscGroupProject;
+
+import java.util.List;
+
 /**
  *
  * @author husseinzahran
  */
 public class Manager extends Employee {
+    private boolean isManager;
+    private List<Employee> employees;
+    
+    /**
+     * @return the isManager
+     */
+    public boolean getIsManager() {
+        return isManager;
+    }
+
+    /**
+     * @param isManager the nationality to set
+     */
+    public void setIsManager(boolean isManager) {
+        this.isManager = isManager;
+    }
+    
+    /**
+     * @return the employees
+     */
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    /**
+     * @param employees the nationality to set
+     */
+    public void setIsManager(List<Employee> employees) {
+        this.employees = employees;
+    }
     
 }
